03540864ea5054848b1c29a4420741f1c567767d
drebalbi <nenhum@nenhum.com>
2025-08-25 18:35:42 -0300

    Moved dbt folder to dag folder

--- dags/simple-dag.py
+++ dags/simple-dag.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 airflow_home = os.environ["AIRFLOW_HOME"]
 
-profile_config_01 = ProfileConfig(  # substitua pelo nome do profile_config adequado *
+profile_config = ProfileConfig(  # substitua pelo nome do profile_config adequado *
     profile_name="project_01",  # substitua pelo nome do projeto (pasta do dbt)
     target_name="prod", # substitua pelo nome do target no seu profiles.yml
     profile_mapping=GoogleCloudServiceAccountFileProfileMapping(
@@ -14,16 +14,16 @@ profile_config_01 = ProfileConfig(  # substitua pelo nome do profile_config adeq
         profile_args={
             "project": "dbt-airflow-469721",  # substitua pelo ID do seu projeto GCP
             "dataset": "prod",  # substitua pelo prefixo (dev_, stg_, prod_) do seu dataset no BigQuery
-            "keyfile": "/usr/local/airflow/dbt/project_01/dbt-airflow-469721-26c5256c1285.json"  # substitua pelo caminho (docker) do seu arquivo de chave de conta de serviço
+            "keyfile": "/usr/local/airflow/dags/dbt/project_01/dbt-airflow-469721-26c5256c1285.json"  # substitua pelo caminho (docker) do seu arquivo de chave de conta de serviço
         }
     )
 )
 
 my_cosmos_dag = DbtDag(
     project_config=ProjectConfig(
-        f"{airflow_home}/dbt/project_01",  # substitua pelo caminho do seu projeto dbt (pasta do projeto dbt)
+        f"{airflow_home}/dags/dbt/project_01",  # substitua pelo caminho do seu projeto dbt (pasta do projeto dbt)
     ),
-    profile_config=profile_config_01,  # substitua pelo nome do profile_config adequado *
+    profile_config=profile_config,  # substitua pelo nome do profile_config adequado *
     execution_config=ExecutionConfig(
         dbt_executable_path=f"{airflow_home}/dbt_venv/bin/dbt",
     ),
