3cf2193c630cbd01873b79e35a2dd614790cf2bf
drebalbi <nenhum@nenhum.com>
2025-08-24 23:18:48 -0300

    Atualizar o parâmetro de agendamento da DAG para executar 4 vezes ao dia

--- dags/simple-dag.py
+++ dags/simple-dag.py
@@ -28,7 +28,7 @@ my_cosmos_dag = DbtDag(
         dbt_executable_path=f"{airflow_home}/dbt_venv/bin/dbt",
     ),
     # normal dag parameters
-    schedule="0 0,6,12,18 * * *",  # 4 vezes por dia
+    schedule="0 */6 * * *", 
     start_date=datetime(2023, 1, 1),  # substitua pela data de inicio
     catchup=False,  # nao ira processar execucoes antigas
     dag_id="simple-dag",  # substitua pelo nome do arquivo .py
